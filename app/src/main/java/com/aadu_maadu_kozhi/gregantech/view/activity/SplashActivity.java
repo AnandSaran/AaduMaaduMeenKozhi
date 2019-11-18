e5d93b53d5fb9d28cb1f23863f0476f1ca43e80a
Anand <s.anandsaravanan@gmail.com>
2019-11-18 12:04:15 +0530

    Splash

--- app/src/main/java/com/aadu_maadu_kozhi/gregantech/view/activity/SplashActivity.java
+++ app/src/main/java/com/aadu_maadu_kozhi/gregantech/view/activity/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        CommonUtils.getInstance().printHashKey(this);
+      //  CommonUtils.getInstance().printHashKey(this);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
